676a7d042f9336569d78d5432facc51e31c46144
dev-akash-sarker <akashsarker210@gmail.com>
2023-09-30 14:32:58 +0600

    gg

--- index.js
+++ index.js
@@ -6,11 +6,53 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.get("/", function (req, res) {
-  res.json({
-    id: "3537c9a0-f85b-49f7-ab01-caa6b6fb0e6a",
-    firstname: "Francisco ",
-    lastname: "B. Sanchez",
-  });
+  res.json([
+    {
+      id: "3537c9a0-f85b-49f7-ab01-caa6b6fb0e6a",
+      firstname: "Francisco ",
+      lastname: "B. Sanchez",
+      email: "FranciscoBSanchez@jourrapide.com",
+      phone: "330-313-5248",
+      password: "12121212",
+      confirmPassword: "12121212",
+      resetPassword: "",
+      resetPasswordToken: "2023-09-28T07:48:12.259G",
+      role: "user",
+      avater:
+        "https://images.pexels.com/photos/9950569/pexels-photo-9950569.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      created: "2023-09-28",
+    },
+    {
+      id: "3537c9a0-f85b-49f7-ab01-caa6b6fb0e6b",
+      firstname: "Douglas ",
+      lastname: "C. Samuel",
+      email: "FranciscoBSanchez@jourrapide.com",
+      phone: "720-275-7943",
+      password: "12121224",
+      confirmPassword: "12121224",
+      resetPassword: "",
+      resetPasswordToken: "2023-09-28T07:48:12.259G",
+      role: "user",
+      avater:
+        "https://images.pexels.com/photos/6968548/pexels-photo-6968548.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      created: "2023-09-28",
+    },
+    {
+      id: "3537c9a0-f85b-49f7-ab01-caa6b6fb0e6c",
+      firstname: "Dennis ",
+      lastname: "D. Brandt",
+      email: "DennisDBrandt@teleworm.us",
+      phone: "508-568-5060",
+      password: "12121258",
+      confirmPassword: "12121258",
+      resetPassword: "",
+      resetPasswordToken: "2023-09-28T07:48:12.259G",
+      role: "user",
+      avater:
+        "https://images.pexels.com/photos/4813340/pexels-photo-4813340.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      created: "2023-09-28",
+    },
+  ]);
 });
 app.get("/users", function (req, res) {
   res.json([
